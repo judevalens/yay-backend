e9346304853396a1678b3e135e405eff8c81feae
judevalens <judevalens@gmail.com>
2021-03-03 22:00:51 -0500

    added keys

---
+++ helpers/keys.go
@@ -0,0 +1,16 @@
+package helpers
+
+const (
+	SpotifyAccessTokenRedirectUri = "http://com.example.yay/"
+	SpotifyClientId               = "c32f7f7b46e14062ba2aea1b462415c9"
+	SpotifyClientSecret           = "229cf90f5da84ba88c3cc5e894699730"
+)
+
+
+const(
+	TwitterApiKey        = "syx9rTlR4L9BwsrgEF1xCmP6q"
+	TwitterSecretKey     = "Gfa9YVZ9gGKehNRKoRuZ9rs78giz9jmhLChW9NnE92BR5Fmkz2"
+	TwitterBearerToken   = "AAAAAAAAAAAAAAAAAAAAANPdKQEAAAAALdI0dQbT3ynlip4AUcJQmdwEWzI%3DAOl175rr8p9VyRYtLWgtavqw2Y1apjaOALThvaTs31sUSAumiV"
+	TwitterAccessToken   = "2919369773-FSemzT7gPFmcrSFAE7qbRLGEuzUdp85dUKZ10Bp"
+	TwitterAccessSecret  = "8KdRCO5kqKwR7vQzDwVBYKUV6ZBDTZt4alceQ0omChqGB"
+)
